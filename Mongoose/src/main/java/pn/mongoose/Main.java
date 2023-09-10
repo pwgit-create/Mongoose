b7af6ab2fef004a8ba627a1a4ef8240dc385f120
pwgit-create <snow_900@outlook.com>
2023-09-10 20:45:44 +0200

    Sip null byte and NodeJSFuzzer

---
+++ Mongoose/src/main/java/pn/mongoose/Main.java
@@ -0,0 +1,15 @@
+package pn.mongoose;
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+     Menu menu = new Menu();
+     menu.printMenu();
+
+    }
+
+ }
